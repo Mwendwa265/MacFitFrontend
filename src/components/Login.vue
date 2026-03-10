216d7059c077aeb9e52c55d5f5d57a8ba328c949
Mwendwa265 <joelwendwa@gmail.com>
2026-03-10 22:00:40 +0300

    my second commit,I have change my login by including a logo

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -14,38 +14,23 @@
 </script>
 
 <template>
-    <v-container width="60%">
+    <v-container width="60%" >
         <v-row>
             <v-col md="12">
                 <v-form>
                     <v-row>
                         <v-col md="12" class="text-center">
-                            <v-icon color="#3A4B68" icon="mdi-weight-lifter" size="large" class="mt-10"></v-icon>
-                        </v-col>
-
-                    </v-row>
-                     <v-row>
-                        <v-col class="text-center">
-                            <div>Welcome to MacFit gym</div>
-                        </v-col>
-                     </v-row>
-
-                    <v-row>
-                        <v-col md="12" class="text-center">
-                            <div  class="text-title-large font-weight-light text-medium text-center" >Username</div>
-                        </v-col>
-                        <v-col md="12">
-                            <v-text-field></v-text-field>
-                        </v-col>
-                    </v-row>
-
-                    <v-row>
-                        <v-col md="12" class="text-center">
-                            <div class="text-title-large font-weight-light text-medium text-center">Password</div>
-                        </v-col>
-                        <v-col md="12">
-                            <v-text-field  
-                                v-model="password"
+                            <!-- <v-icon color="#3A4B68" icon="mdi-FullLogo_Transparent.png" size="large" class="mt-10"></v-icon> -->
+                             <img src="/FullLogo_Transparent_NoBuffer.png" alt="MacFit Gym Logo" class="logo" size="xl" height="20%" />
+                            <div>Welcome to MacFit gym</div><br>
+                            <div  class="text-title-large font-weight-light text-medium text-left" >Username</div>
+                            <v-col md="12">
+                                 <v-text-field></v-text-field>
+                            </v-col>
+                            <div class="text-title-large font-weight-light text-medium text-left">Password</div>
+                            <v-col md="12">
+                                 <v-text-field
+                                 v-model="password"
                                 :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
                                 :rules="[rules.required, rules.min]"
                                 :type="show1 ? 'text' : 'password'"
@@ -53,20 +38,16 @@
                               
                                 counter
                                 @click:append="show1 = !show1"></v-text-field>
-                        </v-col>
-                    </v-row>
+                            </v-col>
+                            <v-col md="4">
+                                <v-btn color="#594976" variant="elevated"class="me-auto">Login</v-btn><br>
 
-                    <v-row>
-                        <v-col md="4">
-                            <v-btn color="#594976" variant="elevated">Login</v-btn>
+                                  <div>new to MacFit gym? <a href="SignUp.vue">sign Up</a></div>
+                            </v-col>
                         </v-col>
+                     
                     </v-row>
-
-                    <v-row>
-                        <v-col md="6">
-                            <div>New to MacFit gym? <a href="SignUp.vue">sign Up</a></div>
-                        </v-col>
-                    </v-row>
+                     
                 </v-form>
             </v-col>
         </v-row>
